28cfc36dfd5c1bb519d6219b0e5b8c62e4bf7c52
DevKang <rkdgusdl92@gmail.com>
2018-02-21 00:41:35 +0900

    [CHANGED] react router 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Board from './pages/Board';
 import './App.css';
+
+import Nav from './common/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ul>
-          <li>home</li>
-          <li>board</li>
-          <li>photo</li>
-        </ul>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route path="/board" component={Board} />
       </div>
     );
   }
