919797f4cec13fe41ebe9a7f66fb8c9d334074a1
EC2 Default User <ec2-user@ip-172-31-8-87.ap-northeast-2.compute.internal>
2018-02-20 13:42:43 +0000

    server init

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+http.createServer(app).listen(9000, () => {
+  console.log('http server running 9000');
+})
+
+https.createServer({
+  pfx: fs.readFileSync('./private/cert.pfx'),
+  passphrase: 'dnflwlq2ro'
+}, app).listen(9443, () => {
+  console.log('https server running 9443');
+});
