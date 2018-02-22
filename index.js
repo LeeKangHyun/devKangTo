8f9c5e35c12d35c7dd3ff58846e73b5275354179
EC2 Default User <ec2-user@ip-172-31-8-87.ap-northeast-2.compute.internal>
2018-02-22 07:36:53 +0000

    [CHANGED] index.js => ssl 지움

--- index.js
+++ index.js
@@ -14,10 +14,3 @@ app.get('*', function (req, res) {
 http.createServer(app).listen(9000, () => {
   console.log('http server running 9000');
 });
-
-https.createServer({
-  pfx: fs.readFileSync(path.join(__dirname, 'private/cert.pfx')),
-  passphrase: 'dnflwlq2ro'
-}, app).listen(9443, () => {
-  console.log('https server running 9443');
-});
