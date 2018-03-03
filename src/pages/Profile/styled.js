179fb40fad35ff83ea78d1bd31e6595f9c0f2bfc
DevKang <rkdgusdl92@gmail.com>
2018-03-03 18:12:36 +0900

    [ADDED] 하히후헤호2

--- src/pages/Profile/styled.js
+++ src/pages/Profile/styled.js
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 import zeze from './image/zeze.jpg';
 
 export const Wrap = styled.div`
-`
+`;
 
 export const Photo = styled.div`
-  width: 600px;
+  max-width: 600px;
   margin: 0 auto;
   padding-bottom: 30px;
   box-sizing: border-box;
@@ -17,16 +17,16 @@ export const Photo = styled.div`
     line-height: 80px;
     background-color: #716961;
   }
-`
+`;
 
 export const Img = styled.div`
-  width: 500px;
+  max-width: 500px;
   height: 480px;
   margin: 45px auto 30px;
   margin-bottom: 20px;
   background: #000 url(${zeze}) center center no-repeat;
   background-size: cover;
-`
+`;
 
 export const TextWrap = styled.div`
   padding: 0 50px;
@@ -34,7 +34,7 @@ export const TextWrap = styled.div`
   > p {
     line-height: 20px;
   }
-`
+`;
 
 export const StatusControl = styled.div`
   text-align: left;
@@ -45,27 +45,27 @@ export const StatusControl = styled.div`
     margin-left: 15px;
     line-height: 35px;
   }
-`
+`;
 
 export const HeartBtn = styled.button`
   float: left;
   height: 35px;
   border: 0;
   background-color: transparent;
-`
+`;
 
 export const HashTag = styled.p`
   color: #2362BF;
   font-style: italic;
   text-align: left;
-`
+`;
 
 export const Text = styled.p`
   color: #161513;
   text-align: left;
-`
+`;
 
 export const Date = styled.p`
   padding-top: 20px;
   text-align: right;
-`
+`;
