cec4fa6a59914d553cac0e0d4ccf454a6b0f1ee0
DevKang <rkdgusdl92@gmail.com>
2018-07-21 17:01:06 +0900

    firebase issue @/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import FB from "firebase";
+import FB from 'firebase/app';
 
 import registerServiceWorker from './registerServiceWorker';
 import Router from './components/Router';
